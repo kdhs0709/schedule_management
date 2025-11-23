589f0504645da3512f6f22a59302a7017f6d1c04
kdhs0709 <73378119+kdhs0709@users.noreply.github.com>
2025-11-23 18:15:59 +0900

    Update lecture.c

--- lecture.c
+++ lecture.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include "lecture.h"
 
-#define MAX_LECTURES 200
-
-Lecture lectureList[MAX_LECTURES];
-int lectureCount = 0;
-
-void loadLectures(const char *filename) {
-    lectureCount = 0;
-    FILE *fp = fopen(filename, "r");
-    if (!fp) {
-        printf("강의 데이터 파일을 읽을 수 없습니다.\n");
-        return;
+int loadLectures(Lecture arr[]) {
+    FILE *fp = fopen("lectures.csv", "r");
+    if(!fp) {
+        printf("[오류] lectures.csv 파일을 찾을 수 없습니다.\n");
+        return 0;
     }
 
-    while (!feof(fp) && lectureCount < MAX_LECTURES) {
-        Lecture *lec = &lectureList[lectureCount];
-
-        fscanf(fp, "%[^,],%[^,],%[^,],%[^,],%[^,],%d,%lf\n",
-               lec->code, lec->name, lec->professor,
-               lec->time, lec->room, &lec->grade, &lec->credit);
-
-        lectureCount++;
+    int i = 0;
+    while(!feof(fp)) {
+        fscanf(fp, "%[^,],%[^,],%[^,],%[^,],%lf\n",
+            arr[i].code, arr[i].name, arr[i].prof,
+            arr[i].time, &arr[i].credit
+        );
+        i++;
     }
 
     fclose(fp);
+    return i;
 }
 
-void printLectures() {
-    printf("\n==== 전체 강의 목록 ====\n");
-    for (int i = 0; i < lectureCount; i++) {
-        Lecture *lec = &lectureList[i];
-        printf("[%d] %s | %s | %s | %s | %d학년 | %.1f학점\n",
-               i + 1,
-               lec->code, lec->name,
-               lec->professor, lec->time,
-               lec->grade, lec->credit);
-    }
-    printf("==========================\n");
+void printLectures(Lecture arr[], int n) {
+    printf("번호 | 학정번호 | 과목명 | 교수 | 시간 | 학점\n");
+    printf("--------------------------------------------------------\n");
+    for(int i=0;i<n;i++)
+        printf("%d | %s | %s | %s | %s | %.1lf\n",
+            i+1, arr[i].code, arr[i].name, arr[i].prof,
+            arr[i].time, arr[i].credit);
 }
