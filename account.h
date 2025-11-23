e0f04fa29d79af9e7b8e609f67d530f712114f21
kdhs0709 <73378119+kdhs0709@users.noreply.github.com>
2025-11-23 18:15:07 +0900

    Update account.h

--- account.h
+++ account.h
@@ -1,7 +1,15 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
-int createAccount();
-int loginAccount();
+typedef struct {
+    char id[50];
+    char pw[50];
+    char name[50];
+    char dept[50];
+    char email[100];
+} Account;
+
+void createAccount();
+int login(Account *loggedIn);
 
 #endif
